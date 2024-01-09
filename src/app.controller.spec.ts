a230e250cc9da62acf5633071d5da74cf447897a
Yago Russo <yagrrusso@gmail.com>
2024-01-08 23:41:33 -0300

    feat: app tests

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+describe('AppController', () => {
+  let controller: AppController
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile()
+
+    controller = module.get(AppController)
+  })
+
+  describe('root', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined()
+    })
+
+    it('should return "Hello World!"', () => {
+      expect(controller.getHello()).toBe('Hello World!')
+    })
+  })
+})
