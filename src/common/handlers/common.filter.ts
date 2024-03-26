525288bec43e21a6564780009045480b1ab3daef
Yago Russo <yagrrusso@gmail.com>
2024-03-26 00:25:48 -0300

    feat: prisma specific error boundary

--- src/common/handlers/common.filter.ts
+++ src/common/handlers/common.filter.ts
@@ -1,5 +1,6 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common'
 import { BaseExceptionFilter } from '@nestjs/core'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 @Catch()
 export class CommonFilter
@@ -10,13 +11,29 @@ export class CommonFilter
     const ctx = host.switchToHttp()
     const res = ctx.getResponse()
 
+    let message = 'Unknown error occurred'
     console.error(exception)
+
+    if (exception instanceof PrismaClientKnownRequestError) {
+      switch (exception.code) {
+        case 'P2000':
+          message = `The provided value for the column ${exception.meta?.target} is too long`
+          break
+        case 'P2002':
+          message = `Unique constraint violation at ${exception.meta?.target}`
+          break
+        case 'P2003':
+          message = `Foreign key constraint violation at ${exception.meta?.target}`
+          break
+        case 'P2025':
+          message = 'Resource not found'
+          break
+      }
+    }
+
     return res.status(+exception?.status || +exception.statusCode || 500).json({
-      data: null,
-      error: {
-        ...exception,
-        message: exception.message,
-      },
+      ...exception,
+      message,
     })
   }
 }
